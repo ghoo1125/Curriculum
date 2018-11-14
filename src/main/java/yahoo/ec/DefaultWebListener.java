334e70b777f08daea8e0226dab87c55ea677e3ba
Louis Chang <hchang01@oath.com>
2018-11-14 13:46:26 +0800

    Create Curriculum with parsec on gradle.

---
+++ src/main/java/yahoo/ec/DefaultWebListener.java
@@ -0,0 +1,22 @@
+package yahoo.ec;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.annotation.WebListener;
+
+import yahoo.ec.parsec_generated.ParsecWebListener;
+
+/**
+ * Default Web listener, replace web.xml.
+ */
+@WebListener
+public class DefaultWebListener extends ParsecWebListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        // Parsec default servlet registrations
+        super.contextInitialized(sce);
+
+        // Add additional servlet filter here
+    }
+}
+
