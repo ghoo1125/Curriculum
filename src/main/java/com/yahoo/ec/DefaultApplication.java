1dc72c990ea130b24292d13fd5c9bd65160e77aa
H.C. Sun <hsinching.sun@oath.com>
2018-12-04 18:10:12 +0800

    feat: Initial commit for `Insert one course` and `list courses by studentId`

---
+++ src/main/java/com/yahoo/ec/DefaultApplication.java
@@ -0,0 +1,31 @@
+package com.yahoo.ec;
+
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+
+import com.yahoo.ec.parsec_generated.ParsecApplication;
+
+/**
+ * This is the entry point of Jersey, which is defined in web.xml.
+ */
+@SuppressWarnings("unused")
+public class DefaultApplication extends ParsecApplication {
+
+    /**
+     * Default constructor.
+     */
+    public DefaultApplication() {
+        // Parsec default bindings and registers
+        super();
+
+        register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                // Add additional binding here
+                // bind(<implementation>.class).to(<interface>.class)
+            }
+        });
+
+        // Add additional register here
+        // register(<resource>.class)
+    }
+}
