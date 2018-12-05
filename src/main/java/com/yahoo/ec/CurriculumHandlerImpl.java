7829867faf94925d3aa4f484953d61a02754e002
Louis Chang <hchang01@oath.com>
2018-12-05 17:40:36 +0800

    Implement getStudents endpoint.

--- src/main/java/com/yahoo/ec/CurriculumHandlerImpl.java
+++ src/main/java/com/yahoo/ec/CurriculumHandlerImpl.java
@@ -9,6 +9,7 @@ import com.yahoo.ec.parsec_generated.Course;
 import com.yahoo.ec.parsec_generated.ResourceContext;
 import com.yahoo.ec.parsec_generated.CurriculumHandler;
 import com.yahoo.ec.parsec_generated.CoursesResponse;
+import com.yahoo.ec.parsec_generated.StudentsResponse;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,6 +22,9 @@ import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.ScanRequest;
 import software.amazon.awssdk.services.dynamodb.model.ScanResponse;
+import software.amazon.awssdk.services.dynamodb.model.QueryRequest;
+import software.amazon.awssdk.services.dynamodb.model.QueryResponse;
+import software.amazon.awssdk.services.dynamodb.model.ConditionalOperator;
 
 import java.net.URISyntaxException;
 import java.net.URI;
@@ -29,6 +33,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * CurriculumHandlerImpl is interface implementation that implement CurriculumHandler interface.
@@ -148,6 +153,61 @@ public class CurriculumHandlerImpl implements CurriculumHandler {
         }
 
         return course;
+    }
+
+    @Override
+    public StudentsResponse getStudents(ResourceContext context, String teacherId) {
+        StudentsResponse res = new StudentsResponse();
+
+        try {
+            // FIXME Should setup DynamoDB with region when deploy to AWS.
+            DynamoDbClient ddb = DynamoDbClient.builder().endpointOverride(
+                new URI("http://localhost:8000")).build();
+
+            // Set up mapping of the partition name with the value.
+            String partitionKeyName = ":tid";
+            HashMap<String, AttributeValue> attrValues =
+                    new HashMap<String,AttributeValue>();
+            attrValues.put(partitionKeyName,
+                AttributeValue.builder().s(teacherId.toString()).build());
+
+            QueryRequest queryReq = QueryRequest.builder()
+                .tableName(COURSE_TABLE_NAME)
+                .keyConditionExpression("TeacherId = " + partitionKeyName)
+                .expressionAttributeValues(attrValues)
+                .build();
+            QueryResponse queryRes = ddb.query(queryReq);
+
+            List<Student> studentList = new ArrayList<Student>();
+            Set<String> studentSet = new HashSet<String>();
+
+            // Iterate over every student in each course item.
+            for (Map<String, AttributeValue> map : queryRes.items()) {
+                List<AttributeValue> students = map.get("Students").l();
+
+                // Remove duplicate students using HashSet.
+                for (AttributeValue s : students) {
+                    studentSet.add(s.s());
+                }
+            }
+
+            for (String s : studentSet) {
+                Student student = new Student();
+                student.setStudentId(s);
+                studentList.add(student);
+            }
+
+            res.setStudents(studentList);
+            res.setResultsTotal(studentList.size());
+        } catch (DynamoDbException e) {
+            System.out.println(e.getMessage());
+            return null;
+        } catch(URISyntaxException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+        return res;
     }
 
     @Override
